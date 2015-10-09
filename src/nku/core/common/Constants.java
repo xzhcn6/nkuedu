bc73d02aa471de4aeadda47de33dcd6f5e3cbd08
shawnzhang <snakeshawnzhang@gmail.com>
2015-10-09 20:42:49 +0800

    【+】Add the file of constants.

---
+++ src/nku/core/common/Constants.java
@@ -0,0 +1,35 @@
+package nku.core.common;
+
+public class Constants {
+	
+	/*
+	 * 成功
+	 */
+	public static final int SUCCESS = 1;
+	/*
+	 * 	成功登陆
+	 */
+	public static final int SUCCESS_LOGIN = 1;
+	/*
+	 * 密码错误
+	 */
+	public static final int WRONG_PWD = 2;
+	/*
+	 * 无此用户
+	 */
+	public static final int NO_SUCH_USER = 3;
+	/*
+	 * 注册成功
+	 */
+	public static final int SUCCESS_REGISTER = 4;
+	/*
+	 * 邮箱已经被注册
+	 */
+	public static final int EMAIL_USED = 5;
+	/*
+	 * 插入失败
+	 */
+	public static final int INSERT_FAILURE = 6;
+	
+	
+}
