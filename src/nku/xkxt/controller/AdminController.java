041b5e869f28d457647687501c0c79b92b76a990
shawnzhang <snakeshawnzhang@gmail.com>
2015-10-27 16:48:55 +0800

    [^]Get student list.

--- src/nku/xkxt/controller/AdminController.java
+++ src/nku/xkxt/controller/AdminController.java
@@ -1,17 +1,22 @@
 package nku.xkxt.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import nku.xkxt.model.Student;
 import nku.xkxt.service.AdminService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 @Controller
 @RequestMapping(value = "/admin")
@@ -33,8 +38,12 @@ public class AdminController {
 	@RequestMapping(value = "/getStudentList")
 	@ResponseBody
 	public Map<String,Object> getStudentList(HttpServletRequest request){
-		Map<String,Object> map = new HashMap<String,Object>();	
-		adminService.getAllStudentByPage();
+		Map<String,Object> map = new HashMap<String,Object>();
+		
+		PageHelper.startPage(1, 10);
+		List<Student> stuList= adminService.getAllStudentByPage();
+		PageInfo<Student> page = new PageInfo<Student>(stuList);
+		map.put("stuList", page);
 		return map;
 	}
 	
