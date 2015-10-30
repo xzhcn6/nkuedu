388c8d8a562e43574fc516a1c141dacc94543cdd
shawnzhang <snakeshawnzhang@gmail.com>
2015-10-31 00:05:24 +0800

    [+]Model of course.

---
+++ src/nku/xkxt/model/Course.java
@@ -0,0 +1,9 @@
+package nku.xkxt.model;
+
+import nku.xkxt.model.base.BaseCourse;;
+
+public class Course extends BaseCourse{
+	
+	private static final long serialVersionUID = 1L;
+	
+}
---
+++ src/nku/xkxt/model/base/BaseCourse.java
@@ -0,0 +1,129 @@
+package nku.xkxt.model.base;
+
+import java.io.Serializable;
+
+public abstract class BaseCourse implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	
+	private String courseNum;
+	
+	private String name;
+	
+	private Integer maxStudent;
+	
+	private String professor;
+	
+	private String classroom;
+	
+	private String request;
+	
+	private String introduction;
+	
+	private String type;
+	
+	private Float credit;
+	
+	private Integer isOpen;
+	
+	private Integer isDelete;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCourseNum() {
+		return courseNum;
+	}
+
+	public void setCourseNum(String courseNum) {
+		this.courseNum = courseNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getMaxStudent() {
+		return maxStudent;
+	}
+
+	public void setMaxStudent(Integer maxStudent) {
+		this.maxStudent = maxStudent;
+	}
+
+	public String getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(String professor) {
+		this.professor = professor;
+	}
+
+	public String getClassroom() {
+		return classroom;
+	}
+
+	public void setClassroom(String classroom) {
+		this.classroom = classroom;
+	}
+
+	public String getRequest() {
+		return request;
+	}
+
+	public void setRequest(String request) {
+		this.request = request;
+	}
+
+	public String getIntroduction() {
+		return introduction;
+	}
+
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Float getCredit() {
+		return credit;
+	}
+
+	public void setCredit(Float credit) {
+		this.credit = credit;
+	}
+
+	public Integer getIsOpen() {
+		return isOpen;
+	}
+
+	public void setIsOpen(Integer isOpen) {
+		this.isOpen = isOpen;
+	}
+
+	public Integer getIsDelete() {
+		return isDelete;
+	}
+
+	public void setIsDelete(Integer isDelete) {
+		this.isDelete = isDelete;
+	}
+	
+}
